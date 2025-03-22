8aab8d05a474cf460302ef2ac45a648da8e1b3c3
Saheel Faisal <saheelfaisal@outlook.com>
2025-03-22 14:24:00 -0500

    Updated risc_cpu.v

--- risc_cpu.v
+++ risc_cpu.v
@@ -3,6 +3,10 @@
 `include "constant_unit.v"
 `include "regfile.v"
 `include "mux_A_B.v"
+`include "function_unit.v"
+`include "data_memory.v"
+`include "mux_C.v"
+`include "mux_D.v"
 
 module risc_cpu (
     input clk, reset
@@ -12,7 +16,9 @@ module risc_cpu (
 
     // IF
     reg [31:0] PC, PC_1, IR;
-    wire [31:0] instruction;
+    wire [31:0] instruction, PC_incremented;
+
+    assign PC_incremented = PC + 1;
     
     instruction_mem inst_mem(
         .PC(PC[13:0]),
@@ -46,7 +52,7 @@ module risc_cpu (
 
     regfile registers(
         .clk(clk), .reset(reset), .RW(RW),
-        .AA(AA_wire), .BA(BA_wire), .DA(DA),
+        .AA(AA_wire), .BA(BA_wire), .DA(DA), .D_data(bus_D_wire),
         .A_data(A_data), .B_data(B_data)
     );
 
@@ -57,18 +63,50 @@ module risc_cpu (
     );
 
     // EX
-    reg RW;
+    reg RW, V_xor_N;
     reg [4:0] DA;
     reg [1:0] MD;
+    reg [31:0] F, data_out;
+
+    wire C, V, N, Z, V_xor_N_wire;
+    wire [1:0] mux_C_sel_wire;
+    wire [31:0] BrA, F_wire, data_out_wire, PC_wire;
+
+    assign BrA = PC_2 + bus_B;
+    assign mux_C_sel_wire = {BS[1], ( ((PS^Z) | BS[1]) & BS[0])};
+
+    function_unit func_unit(
+        .A(bus_A), .B(bus_B), .FS(FS), .SH(SH),
+        .C(C), .V(V), .Z(Z), .N(N), .F(F_wire)
+    );
+
+    data_memory data_mem(
+        .clk(clk), .addr(bus_A[13:0]), .MW(MW), .data_in(bus_B),
+        .data_out(data_out_wire)
+    );
+
+    mux_C muxC(
+        .sel(mux_C_sel_wire), .PC_1(PC_incremented), .BrA(BrA), .RAA(bus_A),
+        .PC(PC_wire)
+    );
+
+    // WB
+    wire [31:0] bus_D_wire;
+
+    mux_D muxD(
+        .MD(MD), .F(F), .data_out(data_out), .V_xor_N({31'b0, V_xor_N}),
+        .bus_D(bus_D_wire)
+    );
 
     always @(posedge clk) begin
         if (reset) begin
-            {PC, PC_1} = 0;
+            {PC, PC_1, IR, PC_2, RW_DOF, DA_DOF, MD_DOF, BS, PS, MW,
+            FS, SH, bus_A, bus_B, RW, DA, MD, V_xor_N, F, data_out} = 0;
         end
 
         else begin
             // IF Stage
-            PC_1 <= PC + 1;
+            PC_1 <= PC_incremented;
             IR <= instruction;
 
             // DOF Stage
@@ -88,10 +126,11 @@ module risc_cpu (
             RW <= RW_DOF;
             DA <= DA_DOF;
             MD <= MD_DOF;
+            
+            V_xor_N <= V_xor_N_wire;
+            F <= F_wire;
+            data_out <= data_out_wire;
 
         end
     end
-    
-    
-
 endmodule 

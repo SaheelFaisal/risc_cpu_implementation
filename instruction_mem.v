0ade9f075d88f148dbbcc3179cd0c9684de44293
Saheel Faisal <saheelfaisal@outlook.com>
2025-03-25 18:55:44 -0500

    updates

--- instruction_mem.v
+++ instruction_mem.v
@@ -31,12 +31,38 @@ endtask
         insert_nops(17);
         memword[20] = {LD, R11, R5, 15'd0};
         insert_nops(21);
-        memword[24] = {ADI, R5, R5, 15'd30};
+        memword[24] = {SUB, R12, R5, R11, 10'd0};   // R5 - R11 = 10 - 25 = -5
         insert_nops(25);
-        memword[28] = {JMP, R0, R0, 15'o77771}; // -1 in 15 bit octadecimal - Jump 1 line up
+        memword[28] = {SLT, R13, R5, R10, 10'd0}; // R13 = 1, if R5 < R10
+        insert_nops(29);
+        memword[32] = {ADI, R8, R8, 15'd5};     // 5 in R8
+        insert_nops(33);
+        memword[36] = {AND, R9, R5, R8, 10'd0}; // 5 & 10 = 0
+        insert_nops(37);
+        memword[40] = {OR, R10, R5, R8, 10'd0}; // 5 | 10 = 15 : R14 = 15
+        insert_nops(41);
+        memword[44] = {XOR, R12, R5, R8, 10'd0}; // 5 ^ 10 = 15: R15 = 15
+        insert_nops(45);
+        memword[48] = {SBI, R10, R11, 15'd3}; // R16 = R11 - 3 = 25 - 3 = 22
+        insert_nops(49);
+        memword[52] = {NOT, R10, R5, 15'd30};
+        insert_nops(53);
+        memword[54] = {ANI, R10, R5, 15'd12}; // 
+        insert_nops(55);
+        memword[58] = {ORI, R10, R5, 15'd10}; // R10 = 15
+        insert_nops(59);
+        memword[62] = {XRI, R10, R5, 15'd15}; // R10 = 5
+        insert_nops(63);
+        memword[66] = {AIU, R10, R5, 15'o77771}; // R10 = 32767 + 5
+        insert_nops(67);
+        memword[70] = {SIU, R10, R5, 15'o77771}; // R10 = 5
+        insert_nops(71);
+        memword[74] = {ADI, R5, R5, 15'd30};
+        insert_nops(75);
+        memword[78] = {JMP, R0, R0, 15'o77771}; // -1 in 15 bit octadecimal - Jump 1 line up
 
 
-        for (i = 29; i < 16384; i = i + 1) begin
+        for (i = 79; i < 16384; i = i + 1) begin
             memword[i] = {NOP, R0, R0, 15'd0};  //32'd0
         end
     end
